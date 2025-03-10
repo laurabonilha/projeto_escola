3bd518ba566fe464e1ad2e6337ccb741f597a6d1
laurabonilha <bonilha.laura@gmail.com>
2025-03-09 21:20:47 -0300

    first commit

---
+++ src/main/java/com/example/projetoescola/controllers/CursoController.java
@@ -0,0 +1,33 @@
+package com.example.projetoescola.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.projetoescola.models.Curso;
+import com.example.projetoescola.repositories.CursoRepository;
+
+
+
+@RestController
+@RequestMapping("cursos")
+public class CursoController {
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping()
+    public List<Curso> obterTodos() {
+        return cursoRepository.obterTodos();
+    }
+    
+    @PostMapping()
+    public Curso salvar (@RequestBody Curso curso){
+        return cursoRepository.salvar(curso);
+    }
+    
+}
